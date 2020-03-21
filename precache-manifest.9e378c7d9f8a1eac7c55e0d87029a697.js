61af0ea4851c87e357cc5c4244a8bc05f91f24d3
letrungkiemai <30624246+letrungkiemai@users.noreply.github.com>
2020-03-21 20:41:58 +0100

    Updates

---
+++ precache-manifest.9e378c7d9f8a1eac7c55e0d87029a697.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47ba42f9c7abfdc374bd1cbab53d7341",
+    "url": "/sortable-table/index.html"
+  },
+  {
+    "revision": "1bcc1dc653763c5660d9",
+    "url": "/sortable-table/static/css/main.c80ad70b.chunk.css"
+  },
+  {
+    "revision": "94b320b0dd2d7f185797",
+    "url": "/sortable-table/static/js/2.ae689e56.chunk.js"
+  },
+  {
+    "revision": "256722dd7b701a66e91d9a4e4a8765ff",
+    "url": "/sortable-table/static/js/2.ae689e56.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1bcc1dc653763c5660d9",
+    "url": "/sortable-table/static/js/main.58bdfbbb.chunk.js"
+  },
+  {
+    "revision": "3b92168d77864b9925f2",
+    "url": "/sortable-table/static/js/runtime-main.9c8e5be3.js"
+  }
+]);
